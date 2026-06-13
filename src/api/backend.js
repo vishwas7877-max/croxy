2d5fd411373993b7ab196b0b3bf59b95243cae17
Vishwas Kumar Singh <vishwaskumarsingh7877@gmail.com>
2026-06-13 23:00:43 +0530

    Configured production frontend API

--- src/api/backend.js
+++ src/api/backend.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
